2e3e4c780a787724eed2348c03ee44c92f2031ef
Yaswanth Narvaneni <yaswanth@gmail.com>
2025-06-18 01:21:16 +0100

    Fix isort import ordering in descriptor module
    Move parent import (from ..) before relative imports (from .) to satisfy isort's import ordering requirements with black profile.

--- agent_uri/descriptor/__init__.py
+++ agent_uri/descriptor/__init__.py
@@ -5,6 +5,8 @@ This package provides utilities to work with agent.json descriptors
 as defined in the agent:// protocol specification.
 """
 
+# Import version from main package
+from .. import __version__  # noqa: F401
 from .compatibility import from_agent_card, to_agent_card  # noqa: F401
 from .generator import AgentDescriptorGenerator  # noqa: F401
 from .models import (  # noqa: F401
@@ -21,6 +23,3 @@ from .validator import (  # noqa: F401
     validate_descriptor,
     validate_required_fields,
 )
-
-# Import version from main package
-from .. import __version__  # noqa: F401
